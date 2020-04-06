527bc6c56c6b0900510f0fbefab9239f63368038
Ryo@RyoPC <cuba6vst@gmail.com>
2020-04-06 18:17:10 +0900

    add channel filter for dev and prod env

--- bot.py
+++ bot.py
@@ -4,17 +4,29 @@ import settings
 
 client = discord.Client()
 
+APP_ENV = settings.APP_ENV
+APP_ENV_PROD = 'prod'
+APP_ENV_DEV = 'dev'
+BOT_TOKEN = settings.BOT_TOKEN
+# 開発検証用チャンネルのID
+TEST_CHANNEL_ID = 696641960047542333
+
 
 @client.event
 async def on_ready():
-    print('Bot is ready!')
+    print('We have logged in as {0.user}'.format(client))
 
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
+    if filterMessage(message):
         return
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
-client.run(settings.BOT_TOKEN)
+
+def filterMessage(message):
+    return message.author.bot or (APP_ENV == APP_ENV_PROD and message.channel.id == TEST_CHANNEL_ID) or (APP_ENV == APP_ENV_DEV and message.channel.id != TEST_CHANNEL_ID)
+
+
+client.run(BOT_TOKEN)
--- settings.py
+++ settings.py
@@ -6,4 +6,5 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+APP_ENV = os.environ.get("APP_ENV")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
